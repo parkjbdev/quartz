c27adfb08d5d2744d99c3a1d2c62f3832e6e05fa
Park JongBeum <dev@parkjb.com>
2024-03-26 20:48:37 +0900

    feat: do not ignore content/.gitignore files

--- quartz/util/glob.ts
+++ quartz/util/glob.ts
@@ -15,7 +15,6 @@ export async function glob(
     await globby(pattern, {
       cwd,
       ignore: ignorePatterns,
-      gitignore: true,
     })
   ).map(toPosixPath)
   return fps as FilePath[]
